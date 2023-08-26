5d568b5bd5c899132c664d82f57dce0e4d5e8d49
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-08-27 10:56:09 +1200

    fix html_project example

--- src/apis/html_project/server.js
+++ src/apis/html_project/server.js
@@ -4,25 +4,28 @@ const fs = require('fs'); // import fs library
 
 const app = express(); // initialise instance of express
 
+
 // middleware function to log all requests
 app.use((req, res, next) => {
-    console.log(`Received request from: ${req.body}`);
+    // console.log(`Received request from: ${req.body}`); body is undefined for GET requests
+    // use req.method to get request method and req.url to get request url
+    console.log(`Received ${req.method} request for ${req.url}}`)
     next();
 });
 
 // middleware function to get path of index.html
 app.get('/', (req, res) => {
     const htmlPage = path.join(__dirname, 'index.html');
-});
 
-// middleware function to send index.html to client
-fs.readFileSync(htmlPage, (err, data) => {
-    if (err) {
-        console.error(err);
-        res.status(500).send('Internal error occurred');
-        return
-    }
-    res.send(htmlPage);
+    // read index.html file and send it to client
+    fs.readFile(htmlPage, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal error occurred');
+            return
+        }
+        res.send(data);
+    });
 });
 
 app.listen(3000, () => {
