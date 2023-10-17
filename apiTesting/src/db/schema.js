55bb598738b3913c64e86d54a2947231972ef59c
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-10-18 11:06:52 +1300

    change schema

---
+++ apiTesting/src/db/schema.js
@@ -0,0 +1,19 @@
+const joi = require('joi');
+
+// create a schema for the student model
+// includes minimum length, maximum length and if required
+const schema = joi.object({
+    name: joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+    address: joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+});
+
+module.exports = schema;
+
+
+
